f8bba5898bdcf3d4a1dbd325474e8f1a61bfa0c7
eleahmloucks <eleahloucks@gmail.com>
2022-08-15 12:59:26 -0600

    initial commit

---
+++ wordcount.py
@@ -0,0 +1,17 @@
+"""Count words in file."""
+def word_count(filename):
+  poem = open(filename)
+
+  #for line in poem
+  print(poem)
+
+
+
+
+word_count('test.txt')
+
+
+
+# put your code here.
+
+
